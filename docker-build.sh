8b60cb4451e5c991d78d36fcbc8259612ffa5060
Serban Gilvitu <serbangilvitu@users.noreply.github.com>
2020-07-06 21:20:48 +0400

    docker-build - avoid repetition

--- docker-build.sh
+++ docker-build.sh
@@ -2,10 +2,12 @@
 
 set -euo pipefail
 
+IMAGE='docker.io/serbangilvitu/wave'
 TAG=$(git rev-parse --verify HEAD)
+
 for jdk in 'jdk8' 'jdk11' 'jdk14'; do
-  docker build . -f docker/${jdk}/Dockerfile -t serbangilvitu/wave:${TAG}
-  docker tag serbangilvitu/wave:${TAG} serbangilvitu/wave:${jdk}
-  docker push serbangilvitu/wave:${TAG}
-  docker push serbangilvitu/wave:${jdk}
+  docker build . -f docker/${jdk}/Dockerfile -t ${IMAGE}:${jdk}-${TAG}
+  docker tag ${IMAGE}:${jdk}-${TAG} ${IMAGE}:${jdk}
+  docker push ${IMAGE}:${jdk}-${TAG}
+  docker push ${IMAGE}:${jdk}
 done
